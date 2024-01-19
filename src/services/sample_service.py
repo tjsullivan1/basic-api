8bfd6f8ccaaa736cbdcf1117462701bb9f96b078
Tim Sullivan <timothyj.sullivan1@gmail.com>
2024-01-18 23:30:56 -0600

    fixed json payload returned by sample service

--- src/services/sample_service.py
+++ src/services/sample_service.py
@@ -1,11 +1,7 @@
 from typing import Optional
 
 
-def make_json_string(sample_string):
-    return f'{{"message": "{sample_string}"}}'
-
-
 async def hello(name: Optional[str] = None):
     if name:
-        return make_json_string("Hello {name}!")
-    return make_json_string("Hello World!")
+        return {"message": f"Hello {name}!"}
+    return {"message": "Hello World!"}
